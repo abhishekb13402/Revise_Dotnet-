71191b29c62a9e36a8c1e9e0f286ee767ebac68c
tntra <tntra@DESKTOP-7LJ1QEL>
2024-06-20 18:28:45 +0530

    implement custom middleware

---
+++ Date-20-6-24/MiddlewareExample/MiddlewareExample/TestMiddleware.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MiddlewareExample
+{
+    public class TestMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("Hello from TestMiddleware app.Map()\n");
+            await next(context); // for next middleware is called
+        }
+    }
+}
